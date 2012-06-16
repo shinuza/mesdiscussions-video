85cbba35d66caf17f5473b7656dd3a43ea41df44
Samori Gorse <samorigorse@gmail.com>
2012-06-16 15:44:40 +0200

    Enable deep linking for youtube videos

--- mesdiscussions-video.user.js
+++ mesdiscussions-video.user.js
@@ -3,13 +3,14 @@
 // @namespace http://noledgedis.com/
 // @description Replace a youtube, dailymotion or vimeo link by an embed video
 // @include http://forum.hardware.fr/*
-// @version 0.1.6-1
+// @version 0.1.7
 // ==/UserScript==
 
 (function() {
   var SELECTOR = '.spoiler .cLink, .messCase2 > div[id] > p > .cLink';
   var HEIGHT = 315;
   var WIDTH = 560;
+  var HASH = /#.*$/;
 
   var matchers = [
     /http(?:s)?\:\/\/(?:www\.)?(youtu)be\.com\/.+v=([\w\-_]+)/,
@@ -38,19 +39,21 @@
     var href = link.href;
     var img = link.querySelector('img');
     var text = link.textContent;
+    var h = href.match(HASH);
 
     matchers.forEach(function(matcher) {
       if(matcher.test(href)) {
         var tokens = href.match(matcher);
         var name = tokens[1]
         var src = tokens[2];
+        var hash = h ? h[0] : '';
 
         var iframe = document.createElement('iframe');
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('allowfullscreen', '');
         iframe.setAttribute('webkitAllowFullScreen', '');
         iframe.setAttribute('mozallowfullscreen', '');
-        iframe.setAttribute('src', roots[name] + src);
+        iframe.setAttribute('src', roots[name] + src + hash);
         iframe.setAttribute('width', WIDTH);
         iframe.setAttribute('height', HEIGHT);
 
